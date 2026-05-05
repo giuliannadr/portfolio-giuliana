7eeb63c07e3431c67330950d271818af7b76c06e
giuliannadr <giulianadirocco1@gmail.com>
2026-05-05 19:46:12 -0300

    insight

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App.tsx'
 import './index.css'
 import './i18n'
 import { ThemeProvider } from './context/ThemeContext'
+import { injectSpeedInsights } from '@vercel/speed-insights'; //
+
+// Inicializar Speed Insights
+injectSpeedInsights(); //
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
